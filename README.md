9f1305c043a272b30e4335051422192bc8f58256
renanAlmeida828 <54690999+renanAlmeida828@users.noreply.github.com>
2020-02-19 08:36:41 -0300

    FrontEnd Files Working

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # maratonaDev3
  Maratona Dev da Rocketseat 3.0
+
+ - **Dia 1:** Construindo o FrontEnd do App DOE
+ - **Dia 2:** Construindo o BackEnd do App DOE
+
+ Link para a página do curso: https://rocketseat.com.br/maratonadev/aulas/3.0
+
+ Esse será meu approach ao app
---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+document.querySelector('#queroAjudarToggle').addEventListener("click", function () {
+  document.querySelector('.form').classList.toggle('hide')
+});
+
+document.querySelector('#queroAjudarToggle').addEventListener("mouseover", function () {
+    document.querySelector('.formTooltip').classList.toggle('tooltipHide')
+  });
+
+
+function tooltipShow (tooltipClass){
+    
+}
